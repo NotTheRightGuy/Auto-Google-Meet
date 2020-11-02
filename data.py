4a27ac1cb3e474940a4026badf1c3e17e8227562
Delsion <janmejaychatterjee@gmail.com>
2020-11-03 00:47:32 +0530

    Make changed in this accordingly

---
+++ data.py
@@ -0,0 +1,10 @@
+email = ''
+password = ''
+
+#You can also remove the dashes from the code, I was just a little lazy
+code = ['qpr-gncp-vxa','rqw-kpoc-hwo','gwy-vcer-pju','wfi-fjrb-oaf','abt-vbiw-wgs'] #Add more code if you have more classes
+timing = ["09:20","10:15","11:00","11:50","12:35"] #Don't forget to add the timing as well
+leave_timing = ["10:11","10:59","11:49","12:34","13:12"] #The timing are in 24 hour format, so make sure you don't mess up
+subject = ['Computer Science','Maths','Chemistry','Physics','English'] #Along with the name, else they code will break :)
+
+
